eb9c9a9bd91fefc64775b177475fb6d707727064
ComicLust <44850790+ComicLust@users.noreply.github.com>
2026-02-23 19:35:29 +0300

    V8

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if($this->app->environment('production') || str_contains(config('app.url'), 'https')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
