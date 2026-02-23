07fb3549a300e828626426f9154f262414b28a5e
ComicLust <44850790+ComicLust@users.noreply.github.com>
2026-02-23 17:43:20 +0300

    v3

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# SQLite veritabanı dosyasını kontrol et ve yoksa oluştur
+if [ ! -f /var/www/database/database.sqlite ]; then
+    echo "Creating database.sqlite..."
+    touch /var/www/database/database.sqlite
+fi
+
+# İzinleri ayarla (Tekrar ve garanti olsun diye)
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache /var/www/database
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache /var/www/database
+
 # Laravel cache temizle
 php artisan optimize:clear
 
